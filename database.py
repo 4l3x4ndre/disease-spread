e8826bc54c3262dc6c74d68409c6f9798d5e1832
4l3x4ndre <alexandre@amrani.fr>
2021-03-24 12:01:03 +0100

    database updates: new database and new class

--- database.py
+++ database.py
@@ -6,9 +6,9 @@ from sqlite3 import Error
 class Database:
 
     def __init__(self, file_name):
-        self.database = str(pathlib.Path().absolute()) + r"\data" + '\\' + file_name + ".db"
+        self.database = str(pathlib.Path().absolute()) + r"\data" + '\\' + file_name + '.db'
+        self.file_name = file_name
         self.conn = self.create_connection()
-        self.create_table()
 
     def create_connection(self):
         """ create a database connection to a SQLite database """
@@ -23,74 +23,36 @@ class Database:
 
         return conn
 
-    def create_table(self):
-        """ create a table from the create_table_sql statement """
-
-        # /!\
-        # contaminated values:
-        # -1 = unreferenced
-        # 0 = not contaminated
-        # 1 = contaminated
-        create_table_sql = """ CREATE TABLE IF NOT EXISTS persons (
-                                        id integer PRIMARY KEY,
-                                        name text NOT NULL,
-                                        firt_name text,
-                                        contaminated int,
-                                        contaminated_date date 
-                                    ); """
-
-        if self.conn is None:
-            print("Error! cannot create the database connection.")
-            return
-
-        try:
-            c = self.conn.cursor()
-            c.execute(create_table_sql)
-        except Error as e:
-            print(e)
-
-    def create_item(self, item_values):
-        """
-        Create a new project into the projects table
-        :return: item id
-        """
-        sql = ''' INSERT INTO persons (name, firt_name, contaminated, contaminated_date)
-                  VALUES(?, ?, ?, ?) '''
-        cur = self.conn.cursor()
-        cur.execute(sql, item_values)
-        self.conn.commit()
-
-        # Return the generated id
-        return cur.lastrowid
-
     def select_item(self):
         """
         Query tasks by priority
         """
         cur = self.conn.cursor()
-        cur.execute("SELECT * FROM persons")
+        cur.execute("SELECT * FROM " + self.file_name)
 
         rows = cur.fetchall()
 
         for row in rows:
             print(row)
 
-    def delete_item(self, item_id):
+    def delete_item(self, item_value, field):
         """
         Delete a task by task id
+        :param item_value: the value of the item to remove
+        :param field: the column name of the value
         """
-        sql = 'DELETE FROM persons WHERE id=?'
+        sql = 'DELETE FROM ' + self.file_name + ' WHERE ' + field + '=?'
         cur = self.conn.cursor()
-        cur.execute(sql, (item_id,))
+        cur.execute(sql, (item_value,))
         self.conn.commit()
 
 
 def main():
 
-    database = Database('mydatabase')
-    database.create_item(('Alexandre', 'Biden', 1, "2021-03-21"))
+    database = Database('trump_edges')
     database.select_item()
-    # database.delete_item(1)
+    database2 = Database('trump_vertices')
+    database2.select_item()
 
 
 if __name__ == '__main__':
